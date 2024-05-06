d674b1194d911334bef75884bd7b0e29bb133e05
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-06 23:42:13 +0700

    add reviews motion and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check, Star } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import Phone from "@/components/Phone";
 import { Icons } from "@/components/Icons";
+import { Reviews } from "@/components/Reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 const page = () => {
   return (
@@ -130,7 +133,7 @@ const page = () => {
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
               What our{" "}
               <span className="relative px-2">
-                Customer{" "}
+                Customers{" "}
                 <Icons.underline className="absolute hidden sm:block pointer-events-none inset-x-0 -bottom-7 text-orange-500" />
               </span>{" "}
               Say
@@ -157,14 +160,14 @@ const page = () => {
               </div>
               <div className="text-lg leading-8 text-justify">
                 <p>
-                  The case feels durable and I even got a compliment from a
-                  friend. I love it so much! Had a great experience with the
+                  &ldquo;The case feels durable and I even got a compliment from
+                  a friend. I love it so much! Had a great experience with the
                   customer service as well. I would{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     recommend this to anyone
                   </span>{" "}
                   who wants a custom case. I&rsquo;m definitely coming back for
-                  a new one soon! 🥰
+                  a new one soon! &ldquo;
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -196,16 +199,13 @@ const page = () => {
               </div>
               <div className="text-lg leading-8 text-justify">
                 <p>
-                  &ldquo;The [Product Name] phone case has been a total
-                  game-changer! I&rsquo;m admittedly pretty clumsy and was
-                  always cracking my phone screens. The textured sides give me a
-                  super{" "}
+                  &ldquo;The anicase phone case has been a total game-changer!
+                  I&rsquo;m admittedly pretty clumsy and was always cracking my
+                  phone screens. The textured sides give me a super{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     secure grip to avoid drops
                   </span>{" "}
-                  in the first place.&rdquo; &nbsp; Despite dropping my phone
-                  multiple times with this case on, there&rsquo;s not a scratch
-                  or dent to be found.{" "}
+                  in the first place. &ldquo;
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -238,8 +238,7 @@ const page = () => {
               <div className="text-lg leading-8 text-justify">
                 <p>
                   &ldquo;The case feels durable and I even got a compliment on
-                  the design. Had the case for two and a half months now
-                  and&rsquo; &rsquo;
+                  the design. Had the case for two and a half months now and{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     the image is super clear
                   </span>
@@ -283,7 +282,7 @@ const page = () => {
                   <span className="p-0.5 bg-slate-800 text-white">
                     looks brand new after about half a year
                   </span>
-                  &nbsp;. I dig it.&rdquo;
+                  &ldquo;
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -304,6 +303,75 @@ const page = () => {
               </div>
             </div>
           </div>
+        </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews />
+        </div>
+      </section>
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{" "}
+                <span className="relative px-2 bg-orange-600 text-white">
+                  your own case{" "}
+                </span>{" "}
+                now
+              </h2>
+            </div>
+          </div>
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <Image
+                src="/arrow.png"
+                alt="arrow"
+                height={70}
+                width={70}
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <Image
+                  src="/horse.jpg"
+                  width={500}
+                  height={500}
+                  alt="horse"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 shrink-0 text-orange-600 inline mr-1.5" />
+              High quality print, durable material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 shrink-0 text-orange-600 inline mr-1.5" />
+              Scratch and fingerprint resistant surface
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 shrink-0 text-orange-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 shrink-0 text-orange-600 inline mr-1.5" />
+              5 years print guarantee
+            </li>
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8 hover:bg-orange-400",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now <ArrowRight className="h-5 w-5 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </div>
