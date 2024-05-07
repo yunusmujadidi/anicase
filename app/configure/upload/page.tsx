2e6207f1e530025f801880b60514a77831d5e33f
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-07 16:00:57 +0700

    add a upload page

--- app/configure/upload/page.tsx
+++ app/configure/upload/page.tsx
@@ -1,5 +1,92 @@
-import React from "react";
+"use client";
+import { Progress } from "@/components/ui/progress";
+import { cn } from "@/lib/utils";
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
+import React, { useState, useTransition } from "react";
+import Dropzone, { FileRejection } from "react-dropzone";
 
-export default function page() {
-  return <div>page</div>;
-}
+const Page = () => {
+  const [isDragOver, setIsDragOver] = useState(false);
+
+  const onDropRejected = () => {
+    console.log("rejected");
+  };
+  const onDropAccepted = () => {
+    console.log("accepted");
+  };
+
+  const isUploading = false;
+  const [isPending, startTransition] = useTransition();
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  return (
+    <div
+      className={cn(
+        "relative flex-1 h-full w-full my-16 rounded-xl lg:rounded-2xl bg-gray-900/5 p-2 ring-inset ring-1 ring-gray-900/10 flex flex-col justify-center items-center",
+        {
+          "ring-blue-900/25 bg-blue-900/10": isDragOver,
+        }
+      )}
+    >
+      <div className="relative flex flex-1 flex-col items-center justify-center w-full">
+        <Dropzone
+          onDropRejected={onDropRejected}
+          onDropAccepted={onDropAccepted}
+          accept={{
+            "image/jpeg": [".jpeg"],
+            "image/png": [".png"],
+            "image/jpg": [".jpg"],
+          }}
+          onDragOver={() => setIsDragOver(true)}
+          onDragLeave={() => setIsDragOver(false)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <div
+              className="h-full w-full flex-1 flex flex-col justify-center items-center"
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              {isDragOver ? (
+                <MousePointerSquareDashed className="w-6 h-6 text-zinc-500 mb-2" />
+              ) : isUploading || isPending ? (
+                <Loader2 className="animate-spin w-6 h-6 text-zinc-500 mb-2" />
+              ) : (
+                // eslint-disable-next-line jsx-a11y/alt-text
+                <Image className="w-6 h-6 text-zinc-500 mb-2" />
+              )}
+              <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+                {isUploading ? (
+                  <div className="flex flex-col items-center">
+                    <p>Uploading...</p>
+                    <Progress
+                      className="mt-2 w-40 h-2 bg-gray-300"
+                      value={uploadProgress}
+                    />
+                  </div>
+                ) : isPending ? (
+                  <div className="flex flex-col items-center">
+                    <p>Redirecting, please wait...</p>
+                  </div>
+                ) : isDragOver ? (
+                  <p>
+                    <span className="font-semibold">Drop file </span>to upload
+                  </p>
+                ) : (
+                  <p>
+                    <span className="font-semibold">Click to upload </span>or
+                    drag and drop
+                  </p>
+                )}
+              </div>
+              {isPending ? null : (
+                <p className="text-xs text-zinc-500">PNG, JPG, JPEG</p>
+              )}
+            </div>
+          )}
+        </Dropzone>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
